4834ef88f27c69ed3d34cefee4d22bccee1750f3
DusaVignesh <vigneshdusa6113@gmail.com>
2023-11-11 04:07:53 +0530

    v1.0

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
-const {getLimit,getUserInput} = require('./inputs');
+const {getLimit,getUserInput, getValue} = require('./inputs');
 const {getAttribute}=require('./play_game');
+const {generateFourDigitUUID}=require('./uuid_generater');
+class GenerateDice{
+    constructor(winData,looseData){
+        this.winData=winData;
+        this.looseData=looseData;
+        this.history=[];
+        this.totalNoOfWins=[...this.winData.values()].reduce((acc,val)=>acc+val,0);
+    }
+    play(){
+        const temp=getAttribute(this.totalNoOfWins,this.winData,this.looseData);
+        this.history.push(temp[1]);
+        if(temp[0]==='W'){
+            const currentCont=this.winData.get(temp[1]);
+            this.winData.set(temp[1],currentCont?currentCont-1:0);
+            this.totalNoOfWins--;
+            console.log(`Won: ${temp[1]}`);
+        }
+        else
+            console.log(`Loose: ${temp[1]}`);
+    }
+    displayHistory(){
+        return this.history;
+    }
+}
+class GenerateId {
+    constructor() {
+      this.lobby = new Map();
+    }
+  
+    async generate() {
+      const winAttributeLimit = await getLimit('Enter Win Limit: ');
+      const winData = await getUserInput(winAttributeLimit);
+      const looseAttributeLimit = await getLimit('Enter Loose Limit: ');
+      const looseData = await getUserInput(looseAttributeLimit);
+  
+      const uuid = generateFourDigitUUID();
+      this.lobby.set(uuid, new GenerateDice(winData, looseData));
+  
+      return uuid;
+    }
+  }
+
+
+class Game{
+    constructor(){
+        this.generateID=new GenerateId();
+    }
+    play(id){
+        const temp=this.generateID.lobby.get(id);
+        temp.play();
+        return temp.displayHistory();
+    }
+    create(){
+        return this.generateID.generate();
+    }
+}
 
 async function main(){
-    // const data=winorloose.fetchData();
-    const winAttributeLimit=await getLimit();
-    const winData = await getUserInput(winAttributeLimit);
-    const looseAttributeLimit=await getLimit();
-    const looseData=await getUserInput(looseAttributeLimit);
-    //   const winData=await getWinData();
-    let totalNoOfWins=[...winData.values()].reduce((acc,val)=>acc+val,0);
-    const arr=[];
-    for(let i=0;i<30;i++){
-        const temp=getAttribute(totalNoOfWins,winData,looseData);
-        arr.push(temp[1]);
-        if(temp[0]==='W'){
-            const currentCont=winData.get(temp[1]);
-            winData.set(temp[1],currentCont?currentCont-1:0);
-            totalNoOfWins--;
+    const diceGame=new Game();
+    let a=true;
+    while(a){
+        const choose = await getLimit('Choose 1: Generate GameID 2: Play Game: 3.Exit: ');
+        switch(choose){
+            case 1:
+                const generatedUUID=await diceGame.create();
+                console.log(`User GameID: ${generatedUUID}`);break;
+            case 2:
+                const uuid=await getValue();
+                const history=diceGame.play(uuid);
+                console.log(`History: ${history}`);break;
+            case 3:
+                a=false;break;
+            default:
+                console.log("try again");
         }
     }
-    console.log(arr);
 }
+
 main();
