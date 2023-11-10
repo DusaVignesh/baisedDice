d4c468efb457185d8ef127c2d5722b92a0de07c0
DusaVignesh <vigneshdusa6113@gmail.com>
2023-11-11 01:55:54 +0530

    implemented core function having dynamic values

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function weightedRandom(choices, probabilities) {
 
 function offer(winloseProb,winData,winProb,lossData,lossProb) {
   const outcome = weightedRandom(['W', 'L'], [winloseProb, 1 - winloseProb]);
-  const choice=outcome==='W'?weightedRandom(winData, winProb):weightedRandom(lossData, lossProb);
+  const choice=outcome==='W'?['W',weightedRandom(winData, winProb)]:['L',weightedRandom(lossData, lossProb)];
   return choice;
 }
 
@@ -21,23 +21,35 @@ const calculateProb=function(total, arr){
     return arr.map((value)=>(value/total));
 }
 
-function main(){
-    //const totalNoOfWins=winorloose.fetchTotal();
-    const totalNoOfWins=30;
+const getAttribute=function(totalNoOfWins,winData,looseData){
     const winloseProb=totalNoOfWins?1/3:0;
+    const winProb=calculateProb(totalNoOfWins,[...winData.values()]);
+    const lossProb=calculateProb(looseData.size,Array(looseData.size).fill(1));
+    return offer(winloseProb,[...winData.keys()],winProb,[...looseData.keys()],lossProb);
+}
+function main(){
     // const data=winorloose.fetchData();
     const winData = new Map([
-        ['10% off', 20],
-        ['20% off', 8],
-        ['30% off', 2]
+        ['10% off', 30],
+        ['20% off', 20],
+        ['30% off', 10]
       ]);
     const looseData = new Map([
         ['better luck next time', null],
         ['try again', null],
         ['retry', null]
       ]);
-    const winProb=calculateProb(totalNoOfWins,[...winData.values()]);
-    const lossProb=calculateProb(looseData.size,Array(looseData.size).fill(1));
-    console.log(offer(winloseProb,[...winData.keys()],winProb,[...looseData.keys()],lossProb));
+    let totalNoOfWins=[...winData.values()].reduce((acc,val)=>acc+val,0);
+    const arr=[];
+    for(let i=0;i<2000;i++){
+        const temp=getAttribute(totalNoOfWins,winData,looseData);
+        arr.push(temp[1]);
+        if(temp[0]==='W'){
+            const currentCont=winData.get(temp[1]);
+            winData.set(temp[1],currentCont?currentCont-1:0);
+            totalNoOfWins--;
+        }
+    }
+    console.log(arr);
 }
 main();
